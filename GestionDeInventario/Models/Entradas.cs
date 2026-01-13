86b293a604c1fbc5db43b536db0d19cd10a9f76e
Romel Martin Ortega Pichardo <romel_ortega@ucne.edu.do>
2026-01-13 15:50:21 -0400

    Se creo el models Entradas

---
+++ GestionDeInventario/Models/Entradas.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestiónDeInventario.Models;
+
+public class Entradas
+{
+    [Key]
+    public int EntradaId { get; set; }
+
+    [Required(ErrorMessage = "La fecha es obligatoria")]
+    public string Concepto { get; set; } = string.Empty;
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    [Required(ErrorMessage = "El concepto es obligatorio")]
+    [StringLength(300, ErrorMessage = "El concepto no puede exceder 300 caracteres")]
+
+    public decimal Total { get; set; }
+
+    [InverseProperty("Entrada")]
+    public virtual ICollection<EntradasDetalle> EntradasDetalle { get; set; } = new List<EntradasDetalle>();
+
+}
