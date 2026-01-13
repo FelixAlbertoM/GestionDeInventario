27c1f29b64d1ee96b7fdfab21854bbad78a1c87c
Romel Martin Ortega Pichardo <romel_ortega@ucne.edu.do>
2026-01-13 15:50:02 -0400

    Se creo el contexto

---
+++ GestionDeInventario/DAL/Contexto.cs
@@ -0,0 +1,54 @@
+﻿using GestiónDeInventario.Data;
+using GestionDeInventario.Data;
+using GestiónDeInventario.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Build.Tasks.Deployment.Bootstrapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestiónDeInventario.DAL
+{
+    public class Contexto : IdentityDbContext<ApplicationUser>
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Entradas> Entradas { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<EntradasDetalle> EntradasDetalle { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Productos>().HasData(
+                new List<Productos>()
+                {
+                new()
+                {
+                    ProductoId = 1,
+                    Descripcion = "Cinta métrica 5 metros",
+                    Costo = 4.00m,
+                    Precio = 7.00m,
+                    Existencia = 0
+                },
+                new()
+                {
+                    ProductoId = 2,
+                    Descripcion = "Taladro eléctrico 1/2 pulgada",
+                    Costo = 45.00m,
+                    Precio = 70.00m,
+                    Existencia = 0
+                },
+                new()
+                {
+                    ProductoId = 3,
+                    Descripcion = "Martillo de acero 16 oz",
+                    Costo = 6.50m,
+                    Precio = 10.00m,
+                    Existencia = 0
+                },
+                }
+            );
+        }
+    }
+}
