e46c1960d02895dca670ee982a2ff26c20c33926
Romel Martin Ortega Pichardo <romel_ortega@ucne.edu.do>
2026-01-14 20:34:47 -0400

    Se corrigio un error de structura

--- GestionDeInventario/Models/Entradas.cs
+++ GestionDeInventario/Models/Entradas.cs
@@ -9,12 +9,11 @@ public class Entradas
     public int EntradaId { get; set; }
 
     [Required(ErrorMessage = "La fecha es obligatoria")]
-    public string Concepto { get; set; } = string.Empty;
     public DateTime Fecha { get; set; } = DateTime.Now;
 
     [Required(ErrorMessage = "El concepto es obligatorio")]
     [StringLength(300, ErrorMessage = "El concepto no puede exceder 300 caracteres")]
-
+    public string Concepto { get; set; } = string.Empty;
     public decimal Total { get; set; }
 
     [InverseProperty("Entrada")]
